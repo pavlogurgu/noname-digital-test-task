1f4754c6b4342a8fcea8b945760d7a3ebb37b52e
pavlogurgu <pavlo.gurgu@gmail.com>
2023-04-10 22:44:11 +0300

    developing users administration

--- src/pages/Users.jsx
+++ src/pages/Users.jsx
@@ -1,17 +1,35 @@
-import data from '../auth/services/dataBackup.json'
-import { nanoid } from 'nanoid'
-
+import data from "../auth/services/dataBackup.json";
 
 export function Users() {
+  console.log(data, typeof data);
 
-    const users = [data]
-
-console.log(users)
-
-
-  return  (
+  return (
     <>
-   {users.map((user) => <h2 key = {nanoid()}>{}</h2>)}
+      {data.users.map((user) => (
+        <>
+          <div class="card" key={user.id} style={{ width: "18rem" }}>
+            <div class="card-body">
+              <h5 class="card-title">{user.name}</h5>
+              {user.admin ? (
+                <p>
+                  <b>Admin</b>
+                </p>
+              ) : null}
+
+              <p class="card-text">Age: {user.age}</p>
+              <p class="card-text">
+                Trips:{" "}
+                {user.trips === "" ? (
+                  <span>No destinations yet</span>
+                ) : (
+                  <span>{user.trips}</span>
+                )}
+              </p>
+              <p class="card-text">Role: {user.role}</p>
+            </div>
+          </div>
+        </>
+      ))}
     </>
-  )
+  );
 }
